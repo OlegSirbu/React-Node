0dfe454c6f41b372dde18c623fc37d473b9b8ef9
olegsyrbu <olegsirbu2@gmail.com>
2023-11-13 10:53:57 +0100

    added check verify with token

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const auth = require("./middleware/auth.js");
 
 require("dotenv").config();
 require("./database/database.js").connect();
@@ -10,6 +11,10 @@ const bodyParser = require("body-parser");
 const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/hello", auth, (req, res) => {
+  res.status(200).send("Hello :) ");
+});
 
 app.use("/api", router);
 
