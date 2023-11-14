ad47ccf522406e08bfed638226017dbd53c38ef4
olegsyrbu <olegsirbu2@gmail.com>
2023-11-14 17:15:39 +0100

    added file checker spell

--- backend/controllers/file.js
+++ backend/controllers/file.js
@@ -1,6 +1,44 @@
 const { File, validate } = require("../models/file");
-
+const fs = require("fs");
+const readline = require("readline");
+const SpellChecker = require("simple-spellchecker").getDictionarySync("en-GB");
+const stringSimilarity = require("string-similarity");
 const BASE_URL = "https://ed-4920280845647872.educative.run";
+
+const spellCheck = async (path) => {
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(path),
+    output: process.stdout,
+    console: false,
+  });
+
+  let text = "";
+
+  for await (const line of readInterface) {
+    const correctedLine = line
+      .split(" ")
+      .map((word) => {
+        if (!SpellChecker.spellCheck(word)) {
+          const suggestions = SpellChecker.getSuggestions(word);
+
+          const matches = stringSimilarity.findBestMatch(
+            word,
+            suggestions.length === 0 ? [word] : suggestions
+          );
+
+          return matches.bestMatch.target;
+        }
+        return word;
+      })
+      .join(" ");
+
+    text += correctedLine + "\n";
+  }
+
+  fs.writeFile(`${path}.txt`, text, (err, res) => {
+    if (err) console.log("error", err);
+  });
+};
 
 exports.upload = async (req, res) => {
   try {
@@ -9,6 +47,11 @@ exports.upload = async (req, res) => {
 
     const { name, description } = req.body;
     let path = req.file.path;
+
+    if (req.file.mimetype === "text/plain") {
+      await spellCheck(req.file.path);
+      path = `${req.file.path}.txt`;
+    }
 
     const file = await File.create({
       name,
